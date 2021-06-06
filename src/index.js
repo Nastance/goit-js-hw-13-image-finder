012fa1f3826c8a3a89106e06bb785a9a5843e80d
Anastasiia Haltseva <galtseva.wait@gmail.com>
2021-06-06 22:29:37 +0300

    completed

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import './css/styles.css';
+import debounce from 'lodash.debounce';
+import ApiService from './js/apiService.js';
+import createImgsCards from './templates/createImgsCards.hbs';
+
+const searchFormRef = document.querySelector('#search-form');
+const loadMoreBtnRef = document.querySelector('[data-action="load-more"]');
+const galleryContainerRef = document.querySelector('.gallery');
+
+const apiService = new ApiService();
+
+searchFormRef.addEventListener('submit', onSearch);
+loadMoreBtnRef.addEventListener('click', onLoadMore);
+
+function onSearch(event) {
+    event.preventDefault();
+
+    
+    apiService.query = event.currentTarget.elements.query.value;
+
+    if (apiService.query === ``) {
+        return alert('Введите название изображения');
+    };
+
+    loadMoreBtnRef.classList.remove('visually-hidden');
+    apiService.resetPage();
+    clearGalleryContainer();
+    apiService.fetchImages().then(appendImageMarkup);
+    
+} 
+
+function onLoadMore() {
+    apiService.fetchImages().then(appendImageMarkup).then(scrollPage);
+}
+
+function appendImageMarkup(imageCard) {
+    galleryContainerRef.insertAdjacentHTML('beforeend', createImgsCards(imageCard))
+}
+
+function clearGalleryContainer() {
+    galleryContainerRef.innerHTML = '';
+}
+
+function scrollPage() {
+     setTimeout(() => {
+    }, 500);
+
+// const element = document.getElementById('.my-element-selector');
+// element.scrollIntoView({
+//   behavior: 'smooth',
+//   block: 'end',
+// });
+}
